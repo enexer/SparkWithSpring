49fb5928f4b934f44bf09e9d08965fb4dcad5608
enexer <as94lol@gmail.com>
2018-07-07 01:45:54 +0200

    Add exceptions, refactor services

---
+++ src/main/java/com/example/demo/services/SparkApplicationService.java
@@ -0,0 +1,84 @@
+package com.example.demo.services;
+
+import com.example.demo.models.MyModel;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.ui.ConsoleProgressBar;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by as on 29.06.2018.
+ */
+@Service
+public class SparkApplicationService {
+
+    @Async
+    public void startTask(UUID uuid, Hashtable<UUID, MyModel> runningTasks) {
+
+        if (runningTasks.get(uuid).getRunning()) {
+            runningTasks.get(uuid).addContent("in progress...");
+            JavaSparkContext jsc = runningTasks.get(uuid).getContext();
+            String res;
+
+
+            jsc.sc().log().info("###################################"+jsc.sc().logName());
+
+            try {
+                res = computePi(jsc);
+            } catch (Throwable t) {
+                runningTasks.get(uuid)
+                        .setRunning(false)
+                        .addContent(t.getMessage());
+                return;
+            }
+
+            ConsoleProgressBar consoleProgressBar = new ConsoleProgressBar(jsc.sc());
+            consoleProgressBar.log().info("HEHEHEE");
+
+            runningTasks.get(uuid).addContent(res);
+        }
+        LocalDateTime time = LocalDateTime.from(LocalDateTime.now());
+
+        runningTasks.get(uuid)
+                .setRunning(false)
+                .addContent("finished")
+                .setFinishTime(time);
+    }
+
+
+    public String computePi(JavaSparkContext jsc) {
+
+        int NUM_SAMPLES = 100;
+        List<Integer> l = new ArrayList<>(NUM_SAMPLES);
+        for (int i = 0; i < NUM_SAMPLES; i++) {
+            l.add(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(i);
+        }
+
+        if (jsc.sc().isStopped()) {
+            return "Cannot call methods on a stopped SparkContext.";
+        }
+
+        long count = jsc.parallelize(l).filter(i -> {
+            double x = Math.random();
+            double y = Math.random();
+            return x * x + y * y < 1.0;
+        }).count();
+        String res = ("Pi is roughly " + 4.0 * count / NUM_SAMPLES);
+
+        //jsc.stop();  /// STOP CONTEXT !!!!!!!!!!!!!!!!!!!!!!!!!
+
+        return res;
+    }
+}
